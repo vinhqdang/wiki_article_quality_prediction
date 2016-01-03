04cb1844fe563563a1a956a303f7547b65e7cfc2
Vinh Q. DANG <dqvinh87@gmail.com>
2016-01-03 13:18:53 +0100

    Add PNN and data

---
+++ pnn/PNN.R
@@ -0,0 +1,75 @@
+seed = 2015
+
+# read data file
+# data file should contains list of features and quality class (FA, GA ...)
+# separate into train and test set
+# the separation depends on seed number (in our case it is 2015)
+# so it is reproducibility
+# splitRatio is the separation split between train and test dataset, default is 80/20
+loadData = function (data_file_name = "../data/article_quality/enwiki.features_wp10.30k.tsv", 
+                     splitRatio= 0.8) 
+{
+  all_data = read.table(data_file_name)
+  
+  library(caTools)
+  
+  sample = sample.split(all_data$V1, SplitRatio = splitRatio)
+  train = subset (all_data, sample==TRUE)
+  test = subset (all_data, sample == FALSE)
+  
+  list(train, test)
+}
+
+# build a model by using pnn package
+buildModel = function (train_frame, sigma = 0.5)
+{
+  y = train_frame[,ncol(train_frame)]
+  train_frame[,ncol(train_frame)] = NULL
+  train_frame = scale(train_frame)
+  train_frame = cbind (as.factor(y), train_frame)
+  library(pnn)
+  fit = pnn::smooth(pnn::learn(data.frame(y, train_frame)), sigma = sigma)
+  fit
+}
+
+# use a model to predict the test value
+performModel = function (model, test_frame) {
+  y = test_frame[,ncol(test_frame)]
+  test_frame[,ncol(test_frame)] = NULL
+  test_frame = scale(test_frame)
+  test_frame = cbind (as.factor(y), test_frame)
+  
+  nbTest = nrow (test_frame)
+  pred = c()
+  actual = c()
+  
+  for (i in 1:nbTest) {
+    print (paste("Element",i))
+    cur_pred = guess (model, as.matrix (test_frame[i,]))
+    if (!is.atomic(cur_pred)) {
+      pred = c(pred, cur_pred$category)
+      actual = c(actual, as.character(y[i]))
+    }
+  }
+  list (actual, pred)
+}
+
+# run everything from beginning
+runAll = function ()
+{
+  print ("Loading data")
+  data = loadData()
+  print ("Building model")
+  model = buildModel (train_frame = data[[1]])
+  print ("Perform the model")
+  model_performance = performModel (model = model, test_frame = data[[2]])
+  
+  # Building confusion matrix 
+  actual = factor(model_performance[[1]], levels = c("stub", "start","c","b","ga","fa"))
+  pred = factor(model_performance[[2]], levels = c("stub", "start","c","b","ga","fa"))
+  t = table (actual, pred)
+  
+  print (t)
+  
+  print (paste("Accuracy =", sum(diag(t))/sum(t)))
+}
---
+++ pnn/README.md
@@ -0,0 +1,35 @@
+# Introduction
+
+This directory contains my implementation to use Probabilistic Neural Network (PNN) to predict the quality of Wikipedia articles.
+
+# Data
+
+Data is provided by Wikimedia Foundation.
+
+## English Wikipedia (enwiki)
+
+The data set of English Wikipedia contains ~ 30 000 articles which are rated (Stub, Start, C, B, GA, FA). Each article is parsed and calculated 24 features.
+
+The list of models can be found [here](https://github.com/wiki-ai/editquality/blob/master/editquality/feature_lists/enwiki.py)
+
+# Implementation
+
+Our implementation is built based on the implementation presented at [1].
+
+## Results (2016 - 01 - 03)
+
+### enwiki
+
+pred
+actual  stub start   c   b  ga  fa
+  stub   925    48   0   0   1   0
+  start   47   919   1   1  35   4
+  c        0     2 878  61   0  29
+  b        0     0  73 896   1   7
+  ga       0    13   4   1 883  66
+  fa       1     2  26   3  21 941
+[1] "Accuracy = 0.924095771777891"
+
+# References
+
+[1] Nigel Lewis (2015), *Build Your Own Neural Network Today!: With step by step instructions showing you how to build them faster than you imagined possible using R*, Create Space publisher. [Amazon](http://www.amazon.com/Build-Your-Neural-Network-Today/dp/1519101236/ref=sr_1_1?ie=UTF8&qid=1451808556&sr=8-1&keywords=build+your+own+neural+network+todays)
